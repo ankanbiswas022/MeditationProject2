ecb7dd1922da8537ba9e034058236048bf9a67fa
Ankan Biswas <ankanbiswas022@gmail.com>
2022-12-16 11:03:47 +0530

    Updated the matched subject List(Gender and Age) as on 16th December

--- getMatchedSubjects.m
+++ getMatchedSubjects.m
@@ -1,51 +1,75 @@
-%  Out put Matrix: (Age Matched subjects)
+% Out-put Matrix: (Age Matched subjects)
+% Total matched and required s of 13th December, 22
+	% Male:   13 Matched, Required: 4 Advanced (A26,A30,A61) and 1 Control (C54)
+	% Female: 11 Matched, Required: 1 Advanced (A42)         and 3 Control (C34, C50, C51, C64)
+
 % 1st and 2nd column: A Male and C male
 % 3rd and 4th Column: A Female and C Female
 
-% Male 20-30
-subjectName{1,1} = '019CKa'; subjectName{1,2} = '022SSP'; % A-23, C-22 , C-22'027SM'
-subjectName{2,1} = '';       subjectName{2,2} = '026HM'; % A- , C-26
-subjectName{3,1} = '012GK';  subjectName{3,2} = ''; % A-28, C- 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Males
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Male 30-40
-subjectName{4,1} = '051RA'; subjectName{4,2} = '028HB'; % A-31, C-33
-subjectName{5,1} = '054MP'; subjectName{5,2} = '043AK'; % A-35, C-34
-subjectName{6,1} = '015RK'; subjectName{6,2} = '071GK'; % A-37, C-39
+% Male 20-29 (2 matched, 1 A26, 1 A30 required)
+subjectName{1,1} = '019CKa'; subjectName{1,2} = '022SSP'; %  A-23,   C-22 , %[C-22 '027SM']
+subjectName{2,1} = '';       subjectName{2,2} = '026HM';  % *A-26 ,  C-26
+subjectName{3,1} = '012GK';  subjectName{3,2} = '093AK';  %  A-28 ,  C-28
+subjectName{4,1} = '';  	 subjectName{4,2} = '075AD';  % *A-30,   C-29 
 
-% Male 40-50
-subjectName{7,1} = '038DK'; subjectName{7,2} = ''; % A-41, C- (*)
-subjectName{8,1} = '053DR'; subjectName{8,2} = '063VK'; % A-43, C-43
+% Male 30-39 (4 matched, 1 with +/-2)
+subjectName{5,1} = '051RA'; subjectName{5,2} = '092KB'; % A-31, C-31
+subjectName{6,1} = '090AV'; subjectName{6,2} = '028HB'; % A-32, C-33
+subjectName{7,1} = '054MP'; subjectName{7,2} = '043AK'; % A-35, C-34
+subjectName{8,1} = '015RK'; subjectName{8,2} = '071GK'; % A-37, C-39
 
-subjectName{9,1} = '045SP'; subjectName{9,2} = ''; % A-47, C-
-subjectName{10,1} = '025RK'; subjectName{10,2} = '070TB'; % A-49, C-49
+% Male 40-44 (2 matched)
+subjectName{9,1}  = '038DK';  subjectName{9,2} = '077LK';  % A-41, C-40 
+subjectName{10,1} = '053DR';  subjectName{10,2} = '063VK'; % A-43, C-43
 
-% Male 50-60
-subjectName{11,1} = '035SS'; subjectName{11,2} = ''; % A-49, C-
-subjectName{12,1} = '010AK'; subjectName{12,2} = '069MG'; % A-54, C-55 , A-54'044PN'
+% Male 45-49 (2 matched)
+subjectName{11,1}  = '045SP'; subjectName{11,2} = '078BM'; % A-47, C-48
+subjectName{12,1} = '025RK';  subjectName{12,2} = '070TB'; % A-49, C-49
 
-% Male 60-70
-subjectName{13,1} = '046ME'; subjectName{13,2} = '068RV'; % A-62, C-61
-% Female 20-30
-subjectName{1,3} = '056PR'; subjectName{1,4} = '073SK'; % A-27, C-26
+% Male 50-59 (2 matched, 1 C54 required)
+subjectName{11,1} = '035SS'; subjectName{11,2} = '080RP'; % A-50, C-50
+subjectName{12,1} = '010AK'; subjectName{12,2} = '069MG'; % A-54, C-55
+subjectName{13,1} = '044PN'; subjectName{13,2} = ''; 	  % A-54, C-
 
-% Female 30-40
-subjectName{2,3} = '052PR'; subjectName{2,4} = ''; % A-31, C-
-subjectName{3,3} = '059MS'; subjectName{3,4} = ''; % A-34, C-(*)
-subjectName{4,3} = '013AR'; subjectName{4,4} = '064PK'; % A-35, C-35 
+% Male 60-65 (1 matched, 1 A61 required)
+subjectName{14,1} = '046ME'; subjectName{14,2} = '068RV'; % A-62, C-61
+subjectName{15,1} = ''; 	 subjectName{15,2} = '085BM'; % A-61, C-61
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Females
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Female 40-50
-subjectName{5,3} = '006SR'; subjectName{5,4} = '062MT'; % A-41, C-42
-subjectName{6,3} = '036MS'; subjectName{6,4} = '049KK'; % A-47, C-46
-subjectName{7,3} = '017KG'; subjectName{7,4} = '072DK'; % A-49, C-49
+% Female 20-29 (2 matched; 1 A23 required)
+subjectName{1,3} = '031BK'; subjectName{1,4} = '081SN'; % A-24, C-23
+subjectName{2,3} = '056PR'; subjectName{2,4} = '073SK'; % A-27, C-26
+subjectName{3,3} = '';      subjectName{3,4} = '086AB'; % *A-28, C-28
 
-% Female 50-60
-subjectName{8,3} = '042VA'; subjectName{8,4} = ''; % A-50, C-
-subjectName{9,3} = '060GV'; subjectName{9,4} = ''; % A-51, C-
-subjectName{10,3} = '050UR'; subjectName{10,4} = ''; % A-53, C-
-subjectName{11,3} = '030SH'; subjectName{11,4} = ''; % A-56, C-
+% Female 30-39 (3 matched, 1 C34 required)
+subjectName{4,3} = '052PR'; subjectName{4,4} = '087KR'; % A-31,  C-31, %[C-31 '082MS']
+subjectName{5,3} = '059MS'; subjectName{5,4} = ''; 	% A-34, *C-34
+subjectName{6,3} = '013AR'; subjectName{6,4} = '064PK'; % A-35,  C-35 
+subjectName{7,3} = '074KS'; subjectName{7,4} = '084AK'; % A-36,  C-37
 
+% Female 40-44 (1 matched,  required A42)
+% *Note: This change was made based on the subject availability 
+subjectName{8,3} = '006SR'; subjectName{8,4} = '088SP'; %  A-40, C-39
+subjectName{9,3} = '';      subjectName{9,4} = '062MT'; % *A-42, C-42
 
-% Female 60-70
-subjectName{12,3} = '008RS'; subjectName{12,4} = ''; % A-64, C-
+% Female 45-49  (2 matched)
+subjectName{10,3}  = '036MS';  subjectName{10,4}  = '049KK';  % A-47, C-46
+subjectName{11,3}  = '017KG';  subjectName{11,4} = '072DK';  % A-49, C-49
+
+% Female 50-59 (2 matched, 2 required C50, C51)
+subjectName{12,3} = '042VA'; subjectName{12,4} = ''; % A-50, *C-50
+subjectName{13,3} = '060GV'; subjectName{13,4} = ''; % A-51, *C-51
+subjectName{14,3} = '050UR'; subjectName{14,4} = '083SP'; % A-53,  C-53
+subjectName{15,3} = '030SH'; subjectName{15,4} = '076BH'; % A-56,  C-56
+
+% Female 60-65 (1 matched with +/-2)
+subjectName{16,3} = '089AB'; subjectName{16,4} = '079SG'; % A-64, C-62
+subjectName{16,3} = '008RS'; subjectName{16,4} = '';  % A-64, *C-64
 
--- getOnlyMatchedSubjects.m
+++ getOnlyMatchedSubjects.m
@@ -1,57 +1,71 @@
 % Out put Matrix: (Age Matched subjects)
 % 1st and 2nd column: A and C; Male and female combined
-% 1-8 rows for male, 9-12 rows for female
+% 1-13 rows for male(n=113), 14-24 rows for female(n=11)
+% Updated on 16th December, assuming all the sujects are good
 
 function subjectNameGrouped = getOnlyMatchedSubjects
 
-% Male 20-30
-subjectNameGrouped{1,1} = '019CKa'; subjectNameGrouped{1,2} = '022SSP'; % A-23, C-22 , C-22'027SM'
-% subjectName{2,1} = '';       subjectName{2,2} = '026HM'; % A- , C-26
-% subjectName{3,1} = '012GK';  subjectName{3,2} = ''; % A-28, C- 
+% Male 20-29 (2 matched, 1 A26, 1 A30 required)
+subjectNameGrouped{1,1} = '019CKa'; 	subjectNameGrouped{1,2} = '022SSP'; %  A-23,   C-22 , %[C-22 '027SM']
+% subjectNameGrouped{2,1} = '*';     	subjectNameGrouped{2,2} = '026HM';  % *A-26 ,  C-26
+subjectNameGrouped{2,1} = '012GK';     subjectNameGrouped{2,2} = '093AK';  %  A-28 ,  C-28
+% subjectNameGrouped{4,1} = '*';  		subjectNameGrouped{4,2} = '075AD';  % *A-30,   C-29 
 
-% Male 30-40
-subjectNameGrouped{2,1} = '051RA'; subjectNameGrouped{2,2} = '028HB'; % A-31, C-33
-subjectNameGrouped{3,1} = '054MP'; subjectNameGrouped{3,2} = '043AK'; % A-35, C-34
-subjectNameGrouped{4,1} = '015RK'; subjectNameGrouped{4,2} = '071GK'; % A-37, C-39
+% Male 30-39 (4 matched, 1 with +/-2)
+subjectNameGrouped{3,1} = '051RA'; subjectNameGrouped{3,2} = '092KB'; % A-31, C-31
+subjectNameGrouped{4,1} = '090AV'; subjectNameGrouped{4,2} = '028HB'; % A-32, C-33
+subjectNameGrouped{5,1} = '054MP'; subjectNameGrouped{5,2} = '043AK'; % A-35, C-34
+subjectNameGrouped{6,1} = '015RK'; subjectNameGrouped{6,2} = '071GK'; % A-37, C-39
 
-% Male 40-50
-% subjectName{7,1} = '038DK'; subjectName{7,2} = ''; % A-41, C-
+% Male 40-44 (2 matched)
+subjectNameGrouped{7,1}  = '038DK';   subjectNameGrouped{7,2} = '077LK';  % A-41, C-40 
+subjectNameGrouped{8,1}  = '053DR';   subjectNameGrouped{8,2} = '063VK'; % A-43, C-43
 
-% subjectNameGrouped{5,1} = '053DR'; subjectNameGrouped{5,2} = '063VK'; % A-43, C-43 % Not taking as some problem in extraction
+% Male 45-49 (2 matched)
+subjectNameGrouped{9,1}  = '045SP';   subjectNameGrouped{9,2} = '078BM'; % A-47, C-48
+subjectNameGrouped{10,1} = '025RK';  subjectNameGrouped{10,2} = '070TB'; % A-49, C-49
 
-% subjectName{9,1} = '045SP'; subjectName{9,2} = ''; % A-47, C-
-subjectNameGrouped{5,1} = '025RK'; subjectNameGrouped{5,2} = '070TB'; % A-49, C-49
+% Male 50-59 (2 matched, 1 C54 required)
+subjectNameGrouped{11,1} = '035SS'; subjectNameGrouped{11,2} = '080RP'; % A-50, C-50
+subjectNameGrouped{12,1} = '010AK'; subjectNameGrouped{12,2} = '069MG'; % A-54, C-55
+% subjectNameGrouped{13,1} = '044PN'; subjectNameGrouped{13,2} = '*'; 	  % A-54, C-
 
-% Male 50-60
-% subjectName{11,1} = '035SS'; subjectName{11,2} = ''; % A-49, C-
-subjectNameGrouped{6,1} = '010AK'; subjectNameGrouped{6,2} = '069MG'; % A-54, C-55 , A-54'044PN'
+% Male 60-65 (1 matched, 1 A61 required)
+subjectNameGrouped{13,1} = '046ME'; subjectNameGrouped{13,2} = '068RV'; % A-62, C-61
+% subjectNameGrouped{15,1} = ''; 	 subjectNameGrouped{15,2} = '085BM'; % A-61, C-61
 
-% Male 60-70
-subjectNameGrouped{7,1} = '046ME'; subjectNameGrouped{7,2} = '068RV'; % A-62, C-61
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Females
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% --------------------------------
-% Female 20-30
-subjectNameGrouped{8,1} = '056PR'; subjectNameGrouped{8,2} = '073SK'; % A-27, C-26
+% Female 20-29 (2 matched; 1 A23 required)
+subjectNameGrouped{14,1} = '031BK'; subjectNameGrouped{14,2} = '081SN'; % A-24, C-23
+subjectNameGrouped{15,1} = '056PR'; subjectNameGrouped{15,2} = '073SK'; % A-27, C-26
+% subjectNameGrouped{3,3} = '*'; 	subjectNameGrouped{3,4} = '086AB'; % *A-28, C-28
 
-% Female 30-40
-% subjectName{2,3} = '052PR'; subjectName{2,4} = ''; % A-31, C-
-% subjectName{3,3} = '059MS'; subjectName{3,4} = ''; % A-34, C-
-subjectNameGrouped{9,1} = '013AR'; subjectNameGrouped{9,2} = '064PK'; % A-35, C-35
+% Female 30-39 (3 matched, 1 C34 required)
+subjectNameGrouped{16,1} = '052PR'; subjectNameGrouped{16,2} = '087KR'; % A-31,  C-31, %[C-31 '082MS']
+% subjectNameGrouped{5,3} = '059MS'; subjectNameGrouped{5,4} = '*'; 	% A-34, *C-34
+subjectNameGrouped{17,1} = '013AR'; subjectNameGrouped{17,2} = '064PK'; % A-35,  C-35 
+subjectNameGrouped{18,1} = '074KS'; subjectNameGrouped{18,2} = '084AK'; % A-36,  C-37
 
+% Female 40-44 (1 matched,  required A42)
+% *Note: This change was made based on the subject availability 
+subjectNameGrouped{19,1} = '006SR'; subjectNameGrouped{19,2} = '088SP'; %  A-40, C-39
+% subjectNameGrouped{9,3} = '*'; 	subjectNameGrouped{9,4} = '062MT'; % *A-42, C-42
 
-% Female 40-50
-subjectNameGrouped{10,1} = '006SR'; subjectNameGrouped{10,2} = '062MT'; % A-41, C-42
-subjectNameGrouped{11,1} = '036MS'; subjectNameGrouped{11,2} = '049KK'; % A-47, C-46
-subjectNameGrouped{12,1} = '017KG'; subjectNameGrouped{12,2} = '072DK'; % A-49, C-49
+% Female 45-49  (2 matched)
+subjectNameGrouped{20,1}  = '036MS';  subjectNameGrouped{20,2}  = '049KK';  % A-47, C-46
+subjectNameGrouped{21,1}  = '017KG';  subjectNameGrouped{22,2} = '072DK';  % A-49, C-49
 
-% Female 50-60
-% subjectName{8,3} = '042VA'; subjectName{8,4} = ''; % A-50, C-
-% subjectName{9,3} = '060GV'; subjectName{9,4} = ''; % A-51, C-
-% subjectName{10,3} = '050UR'; subjectName{10,4} = ''; % A-53, C-
-% subjectName{11,3} = '030SH'; subjectName{11,4} = ''; % A-56, C-
+% Female 50-59 (2 matched, 2 required C50, C51)
+% subjectNameGrouped{12,3} = '042VA'; subjectNameGrouped{12,4} = '*C-50'; % A-50, *C-50
+% subjectNameGrouped{13,3} = '060GV'; subjectNameGrouped{13,4} = '*C-51'; % A-51, *C-51
+subjectNameGrouped{22,1} = '050UR'; subjectNameGrouped{22,2} = '083SP'; % A-53,  C-53
+subjectNameGrouped{23,1} = '030SH'; subjectNameGrouped{23,2} = '076BH'; % A-56,  C-56
 
-
-% Female 60-70
-% subjectName{12,3} = '008RS'; subjectName{12,4} = ''; % A-64, C-
+% Female 60-65 (1 matched with +/-2)
+subjectNameGrouped{24,1} = '089AB'; subjectNameGrouped{24,2} = '079SG'; % A-64, C-62
+% subjectNameGrouped{16,3} = '008RS'; subjectNameGrouped{16,4} = '*C64';  % A-64, *C-64
 
 end
