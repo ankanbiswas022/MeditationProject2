3c1d66b5ac2967cad2af7e1a73d324d7c624f87f
Ankan Biswas <ankanbiswas022@gmail.com>
2023-04-06 12:10:04 +0530

    Reversing the display code to the orginal status to display the meditation data for single subjects

--- runDisplayMeditationData.m
+++ runDisplayMeditationData.m
@@ -1,128 +1,46 @@
-% to make a summary plot for all subjects
+% subjectName='008RS';
+% expDate='100122';
 clear; close all
+
 [subjectNames,expDates] = subjectDatabaseMeditationProject2;
 folderSourceString='D:\Projects\MeditationProjects\MeditationProject2';
-% folderSourceString = 'C:\Users\Srishty\OneDrive - Indian Institute of Science\Documents\supratim\meditationProject';
 badElectrodeList=[];
 plotRawTFFlag=[];
-badTrialNameStr='_d4020_v10';
+badTrialNameStr='_v5'; 
 badElectrodeRejectionFlag=1;
-saveFileFlag = 0;
+saveFileFlag = 1;
+refScheme = 1; % 2 for bipolar
 saveDataIndividualSubjectsFlag = 0; % if the flag is on the script would save the data for the given subject list
-individualSubjectFigureFlag = 0;  %the flag for bad trial figure for indiviual subjects 
+% this could not have been included in the segmentAndSaveData because
+timeAvgFlag = 1; 
+tfFlag = 1; 
+% --------------------------------------------------------------------
 
-% segmentTheseIndices =[3:17 19:28 30:34 36:44 46]; %29 some problem
-% segmentTheseIndices = [3:17 19:34 36:44 46] ;
-allBadElecs = [];   
-badElecsGroupWise = [];
+% displayMeditationData(subjectName,expDate,folderSourceString,badTrialNameStr,badElectrodeRejectionFlag,plotRawTFFlag)
 
-% segmentTheseIndices = [3,4,5]; %29 some problem
-% segmentTheseIndices = [24]; %29 some problem
-segmentTheseIndices = [3:10]; %29 some problem
+% segmentTheseIndices = [3:17 19:25];
+segmentTheseIndices = 13;
 
 for i=1:length(segmentTheseIndices)
-    
-    h=figure(segmentTheseIndices(i));
+    if ~saveDataIndividualSubjectsFlag     
+        h=figure(segmentTheseIndices(i)+1); 
+    end
     
     subjectName = subjectNames{segmentTheseIndices(i)};
     expDate = expDates{segmentTheseIndices(i)};
-    
-    [allBadElecs(i,:),badElecsGroupWise(i,:,:)] = dispBadElecTrialsAcrossProtocols(subjectName,expDate,folderSourceString,badTrialNameStr,individualSubjectFigureFlag); %working on this
-   
+    %     displayMeditationDataV1(subjectName,expDate,folderSourceString,badTrialNameStr,plotRawTFFlag); %first version
+    %     displayMeditationDataV2(subjectName,expDate,folderSourceString,badTrialNameStr,badElectrodeRejectionFlag,plotRawTFFlag);
+    %     displayMeditationData(subjectName,expDate,folderSourceString,badTrialNameStr,badElectrodeRejectionFlag,plotRawTFFlag); %working on this
+    displayMeditationDataV3(subjectName,expDate,folderSourceString,badTrialNameStr,badElectrodeRejectionFlag,plotRawTFFlag,refScheme,timeAvgFlag,saveDataIndividualSubjectsFlag,tfFlag); %working on this
+%     savePowerDataMeditation(subjectName,expDate,folderSourceString,badTrialNameStr,badElectrodeRejectionFlag,plotRawTFFlag,refScheme,saveDataIndividualSubjectsFlag)
+    %     displayMeditationData(subjectName,expDate,folderSourceString,badTrialNameStr,badElectrodeRejectionFlag,plotRawTFFlag) %working on this
+        
+%     set(h,'outerPosition',[0 0 1 1],'unit','normalized')
+if saveFileFlag
+    fileName = [subjectName '_DisplayResultsV3' '.fig'];    
+    fileNameTif = [subjectName '_DisplayResultsV3'];
+    h.WindowState = 'maximized';
+    saveas(h,fileName);
+    print(h,fileNameTif,'-dtiff','-r600');
 end
-
-
-% %%data for plotting
-% 
-% %%%fixed variables%%%%%%
-% allElectrodeList = 1:64;
-% numElectrodes = length(allElectrodeList);
-% protocolNameList = [{'EO1'}     {'EC1'}     {'G1'}      {'M1'}          {'G2'}      {'EO2'}     {'EC2'}     {'M2'}];
-% numProtocols = length(protocolNameList);
-% 
-% [~,~,~,electrodeGroupList,groupNameList,highPriorityElectrodeNums] = electrodePositionOnGrid(64,'EEG',subjectName,6);
-%     electrodeGroupList{length(electrodeGroupList)+1} = highPriorityElectrodeNums;
-%     groupNameList{length(groupNameList)+1} = 'highPriority';
-% 
-%  proportionAllBadElecs = allBadElecs./length(allElectrodeList);
-% 
-%  for k=1:length(electrodeGroupList)
-%      proportionBadElecsGroupWise(:,:,k) = badElecsGroupWise(:,:,k)./length(electrodeGroupList{k});
-%  end
-% 
-% for j=1:numProtocols
-%     subplot(3,4,j)
-%     stem(segmentTheseIndices,proportionAllBadElecs(:,j),'color',[0.5 0.5 0.5]); axis('tight'); hold on
-%     stem(segmentTheseIndices,proportionBadElecsGroupWise(:,j,6),'color',[0.8 0 0]); axis('tight'); hold on %high priority
-%     stem(segmentTheseIndices,proportionBadElecsGroupWise(:,j,4),'color',[0 0.8 0]); axis('tight'); hold on %frontal
-%     yline(0.3,'--');
-%     xlim([1 segmentTheseIndices(end)+1]); ylim([0 1]);
-%     title(protocolNameList{j});
-%     if j==4
-%         legend('All','High Priority','Frontal','location','best');
-%     elseif j==5
-%         xlabel('Subject Index');  ylabel('Proportion of Bad Electrode');
-%     end
-% end
-% 
-% sgtitle('Bad Electrodes across Subjects','FontSize',18);
-% 
-% thresholdForBadSubjetRejection = [0.1:0.1:1]; % if this proportion of electrodes are bad, then subjeect is rejected
-% numThreshold = length(thresholdForBadSubjetRejection);
-% goodSubjects = zeros(numThreshold,length(segmentTheseIndices));
-% goodSubjectsFrontal = zeros(numThreshold,length(segmentTheseIndices));
-% goodSubjectsHighPriority = zeros(numThreshold,length(segmentTheseIndices));
-% goodSubjectsCommon = zeros(numThreshold,length(segmentTheseIndices));
-% 
-%  for m = 1:numThreshold
-%      for i=1:length(segmentTheseIndices)
-%          if isempty(find(proportionAllBadElecs(i,:)>thresholdForBadSubjetRejection(m)))
-%             goodSubjects(m,i) = 1;
-%          end
-% 
-%          if isempty(find(proportionBadElecsGroupWise(i,:,4)>thresholdForBadSubjetRejection(m)))
-%             goodSubjectsFrontal(m,i) = 1;
-%          end
-% 
-%          if isempty(find(proportionBadElecsGroupWise(i,:,6)>thresholdForBadSubjetRejection(m)))
-%             goodSubjectsHighPriority(m,i) = 1;
-%          end
-% 
-%          if isempty(find(proportionBadElecsGroupWise(i,:,6)>thresholdForBadSubjetRejection(m))) && isempty(find(proportionBadElecsGroupWise(i,:,6)>thresholdForBadSubjetRejection(m)))% isempty(find(proportionAllBadElecs(i,:)>thresholdForBadSubjetRejection(m))) &&
-%             goodSubjectsCommon(m,i) = 1;
-%          end
-%      end
-%  end
-% 
-%  numGoodSubjects = sum(goodSubjects,2);
-%  numGoodSubjectsFrontal = sum(goodSubjectsFrontal,2);
-%  numGoodSubjectsHighPriority = sum(goodSubjectsHighPriority,2);
-%  numGoodSubjectsCommon = sum(goodSubjectsCommon,2);
-% 
-% h1= subplot(3,4,9);
-% plot(thresholdForBadSubjetRejection,numGoodSubjects);
-% xline(0.5,'--');
-% xlabel('threshold');
-% ylabel('No. of good subjects');
-% title('All Electrodes');
-% 
-% h2 = subplot(3,4,10);
-% plot(thresholdForBadSubjetRejection,numGoodSubjectsFrontal);
-% xline(0.5,'--');
-% xlabel('threshold');
-% title('Frontal');
-% 
-% h3 = subplot(3,4,11);
-% plot(thresholdForBadSubjetRejection,numGoodSubjectsHighPriority);
-% xline(0.5,'--');
-% xlabel('threshold');
-% title('High Priority');
-% 
-% h4 = subplot(3,4,12);
-% plot(thresholdForBadSubjetRejection,numGoodSubjectsCommon);
-% xline(0.5,'--');
-% xlabel('threshold');
-% title('Frontal + HighPriority');
-% 
-% linkaxes([h1 h2 h3 h4]);
-% ylim([0 length(segmentTheseIndices)+1]);
+end
