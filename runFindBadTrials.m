08e86cb2ae2da8f163ec01f405db9c5091924d79
Ankan Biswas <ankanbiswas022@gmail.com>
2023-08-22 09:51:50 +0530

    Saving the snapshot of the code which was used to run the bad trial for all the subjects for two protocols, 'EO1' and 'EC1' with having the same time period as the checkPeriod, [-1 1.25];

--- runFindBadTrials.m
+++ runFindBadTrials.m
@@ -8,7 +8,7 @@ folderSourceString = 'D:\Projects\MeditationProjects\MeditationProject2';
 
 % doing everything for the first 25 subjects:
 problamaticSubjectIndex = 53;
-segmentTheseIndices = setdiff(3:66,problamaticSubjectIndex) ; % total list as of 6/9/22
+segmentTheseIndices = setdiff(3:101,problamaticSubjectIndex) ; % total list as of 6/9/22
 % segmentTheseIndices = setdiff(3:66,problamaticSubjectIndex) ; % total list as of 6/9/22
 
 gridType = 'EEG';
@@ -16,7 +16,7 @@ nonEEGElectrodes = 65:80;
 impedanceTag = '_Impedance_Start';
 capType = 'actiCap64_UOL';
 saveDataFlag = 1;
-badTrialNameStrS = {'_d4020_v10'}; % with the 5 deg window
+badTrialNameStrS = {'_v8_b30d5_cb'}; % with the 5 deg window
 % badTrialNameStr = '_d4020_v7_flatPsd'; % check once again, '_d4020_v7_flatPsd';
 % badTrialNameStr = '_d4020'; % check once again, '_d4020_v7_flatPsd';
 % badTrialNameStrS = {'_d4020_v7'}; % check once again, '_d4020_v7_flatPsd';
@@ -24,11 +24,11 @@ displayResultsFlag = 0;
 electrodeGroup = ''; % by default, highPriorityElectrodes are used in this case
 % checkPeriod = [-1.25 1.25];
 checkPeriod = [-1 1.25]; % modifiedCheckPeriod after excluding the initial 250ms which might have lots of bad eye epochs
-checkBaselinePeriod = [-1 0];
+checkBaselinePeriod = checkPeriod;
 badEEGElectrodes = [];
 
-useEyeData =       [1       0       1      1      1      1       0       1     ];
-protocolNameList = [{'EO1'} {'EC1'} {'G1'} {'M1'} {'G2'} {'EO2'} {'EC2'} {'M2'}];
+useEyeData =       [1       0       ];
+protocolNameList = [{'EO1'} {'EC1'} ];
 
 % useEyeData =       [1       0     1      1      1       0       1     ];
 % protocolNameList = [{'EO1'} {'EC1'} {'M1'} {'G2'} {'EO2'} {'EC2'} {'M2'}];
@@ -47,7 +47,7 @@ for b = 1:length(badTrialNameStrS)
             useEyeDataFlag = useEyeData(j);
             %             findBadTrialsWithEEG_v10(subjectName,expDate,protocolName,folderSourceString,gridType, ...
             %                 badEEGElectrodes,nonEEGElectrodes,impedanceTag,capType,saveDataFlag,badTrialNameStr,displayResultsFlag,electrodeGroup,checkPeriod,checkBaselinePeriod,useEyeData);
-            findBadTrialsWithEEG_v10(subjectName,expDate,protocolName,folderSourceString,gridType,badEEGElectrodes,...
+            findBadTrialsWithEEG(subjectName,expDate,protocolName,folderSourceString,gridType,badEEGElectrodes,...
     nonEEGElectrodes,impedanceTag,capType,saveDataFlag,badTrialNameStr,displayResultsFlag,electrodeGroup,checkPeriod,checkBaselinePeriod,useEyeDataFlag);
 
         end
