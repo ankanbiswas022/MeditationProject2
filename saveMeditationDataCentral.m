46bc571b72623081a9513da48890fa079f000e6e
Ankan Biswas <ankanbiswas022@gmail.com>
2023-09-27 14:34:20 +0530

    Adding the central data saving script for the Meditation Project

---
+++ saveMeditationDataCentral.m
@@ -0,0 +1,66 @@
+% Central Data saving code fo rthe meditation Subject
+% Ankan
+% Target Completion: 5.30-8.30 am (Max)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% load the subject list
+sourcePath = 'N:\Projects\MeditationProjects\MeditationProject2\Codes\codesUnderDevelopment\UpdatedPrograms\badSubjectCodes';
+fileName = 'SubjectListV1.mat';
+load(fullfile(sourcePath,fileName),'allMatchedSubjectList');
+
+% get the Indexes of the matched subject
+
+[subjectNames,expDates] = subjectDatabaseMeditationProject2;
+allIndexes=[];
+for i=1:length(allMatchedSubjectList)
+    strToFind = allMatchedSubjectList{i};
+    disp(allMatchedSubjectList{i});
+    ind=find(strcmp(strToFind,subjectNames));
+    disp(ind);
+    allIndexes = [allIndexes ind];
+end
+
+allMatchedSubjectIndex = allIndexes';
+saveTheseIndices = allMatchedSubjectIndex(1:end);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% wrapping flags and input parameters
+sdParams.folderSourceString = 'D:\Projects\MeditationProjects\MeditationProject2';
+sdParams.subjectNames = subjectNames;
+sdParams.expDates = expDates;
+sdParams.saveTheseIndices = saveTheseIndices;
+sdParams.badTrialNameStr = '_v8_b30d5';
+sdParams.badElectrodeRejectionFlag = 2; % 1: saves all the electrodes, 2: rejects individual protocolwise 3: rejects common across all protocols
+sdParams.logTransformFlag = 0; % saves log Transformed PSD if 'on'
+sdParams.saveDataFlag = 1; % if 1, saves the data
+sdParams.eegElectrodeList = 1:64;
+sdParams.freqRange = [0 250];
+sdParams.biPolarFlag = 0;
+sdParams.removeIndividualUniqueBadTrials=0;
+sdParams.removeVisualInspectedElecs = 0;
+
+sdParams.folderSourceString = 'D:\Projects\MeditationProjects\MeditationProject2';
+saveDataDeafultStr ='subjectWise';
+if sdParams.biPolarFlag
+    saveFolderName = [saveDataDeafultStr 'Bipolar'];
+else
+    saveFolderName = [saveDataDeafultStr 'Unipolar'];
+end
+sdParams.saveDataFolder = fullfile(sdParams.folderSourceString,'data','savedData',saveFolderName);
+saveFileNameDeafultStr = ['_unipolar_stRange_250_1250' sdParams.badTrialNameStr '.mat'] ;
+
+
+% Not extracted 46
+
+for i=47:length(saveTheseIndices)
+    subjectName = subjectNames{saveTheseIndices(i)};
+    disp(['Working on the '  subjectName]);
+    expDate = expDates{saveTheseIndices(i)};
+
+    sdParams.saveFileName = [subjectName saveFileNameDeafultStr];
+    saveIndividualSubjectDataMeditationProtocolWise(subjectName,expDate,sdParams);
+end
+
+
+
+
